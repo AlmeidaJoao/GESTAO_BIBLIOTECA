0fa59b0d52c047d885789adf4b74f7d3acc80bbc
Nozotrox <Feliciano_jr.mazoio@yahoo.com>
2019-10-04 00:44:53 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/Gestao_Biblioteca.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Gestao_Biblioteca.dir/main.cpp.obj"
+  "Gestao_Biblioteca.pdb"
+  "Gestao_Biblioteca.exe"
+  "Gestao_Biblioteca.exe.manifest"
+  "libGestao_Biblioteca.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Gestao_Biblioteca.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
