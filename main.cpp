73d32fa2e5552c262c87228ed54e7596b169c4be
Nozotrox <Feliciano_jr.mazoio@yahoo.com>
2019-11-11 21:34:47 +0200

    Todas operacoes feitas

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@ void lerFicheiro();
 void inserirDadosPreliminares();
 void imprimirMenu();
 void usarOpcao(int i);
+void loadBackup();
 
 int main() {
 
@@ -45,40 +46,44 @@ int main() {
     return 0;
 }
 
+void loadBackup(){
+    Livro* doc2 = new Livro("Jealousy","magna vestibulum aliquet ultrices erat tortor sollicitudin mi sit amet lobortis","Gorczany, Larkin and Weber",2006,5,"039209482A", 20);
+    Livro* doc = new Livro("Hunter Prey","metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit amet diam in magna bibendum","Torphy and Sons",1988,2, "039209482A", 20);
+    Livro* doc1 = new Livro("The 19th Wife"," The facilisi cras non velit nec nisi vulputate nonummy maecenas tincidunt lacus at velit vivamus vel nulla","Corwin Inc",2005,3, "039209482A", 20);
+    Livro* doc3 = new Livro("Silent Witness (Do Not Disturb)","pede ac diam cras pellentesque volutpat dui maecenas tristique est et tempus semper est quam pharetra magna","Littel LLC",1992,7, "039209482A", 20);
+    Livro* doc4 = new Livro("Have Rocket, Will Travel","magnis dis parturient montes nascetur ridiculus mus etiam vel augue vestibulum rutrum rutrum neque","Cruickshank, Monahan and Crooks",2010,1, "039209482A", 20);
+    Livro* doc5 = new Livro("Baadasssss! (How to Get the Man's Foot Outta Your Ass)","pellentesque quisque porta volutpat erat quisque erat eros viverra eget", "Hauck Inc",2012,4, "039209482A", 4);
+
+    Autor* autor = new Autor("Van Hauten");
+    Autor* autor2 = new Autor("Van Diesel");
+    Autor* autor3 = new Autor("Van Gasoline");
+
+    string name1 = typeid(doc).name();
+    string name2 = typeid(doc).name();
+
+    doc->setAutor(autor);
+    doc1->setAutor(autor);
+    doc2->setAutor(autor2);
+    doc3->setAutor(autor3);
+    doc4->setAutor(autor3);
+    doc5->setAutor(autor3);
+
+    bib->inserirAutor(autor);
+    bib->inserirAutor(autor2);
+    bib->inserirAutor(autor3);
+
+
+    bib->inserirLivro(doc2);
+    bib->inserirLivro(doc);
+    bib->inserirLivro(doc1);
+    bib->inserirLivro(doc3);
+    bib->inserirLivro(doc4);
+    bib->inserirLivro(doc5);
+
+}
+
 void inserirDadosPreliminares(){
 
-    //Livro* doc2 = new Livro("Jealousy","magna vestibulum aliquet ultrices erat tortor sollicitudin mi sit amet lobortis","Gorczany, Larkin and Weber",2006,10,"039209482A", 20);
-    //Livro* doc = new Livro("Hunter Prey","metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit amet diam in magna bibendum","Torphy and Sons",1988,10, "039209482A", 20);
-    //Livro* doc1 = new Livro("The 19th Wife"," The facilisi cras non velit nec nisi vulputate nonummy maecenas tincidunt lacus at velit vivamus vel nulla","Corwin Inc",2005,10, "039209482A", 20);
-    //Livro* doc3 = new Livro("Silent Witness (Do Not Disturb)","pede ac diam cras pellentesque volutpat dui maecenas tristique est et tempus semper est quam pharetra magna","Littel LLC",1992,10, "039209482A", 20);
-    //Livro* doc4 = new Livro("Have Rocket, Will Travel","magnis dis parturient montes nascetur ridiculus mus etiam vel augue vestibulum rutrum rutrum neque","Cruickshank, Monahan and Crooks",2010,10, "039209482A", 20);
-    //Livro* doc5 = new Livro("Baadasssss! (How to Get the Man's Foot Outta Your Ass)","pellentesque quisque porta volutpat erat quisque erat eros viverra eget", "Hauck Inc",2012,10, "039209482A", 20);
-
-//    Autor* autor = new Autor("Van Hauten");
-//    Autor* autor2 = new Autor("Van Diesel");
-//    Autor* autor3 = new Autor("Van Gasoline");
-
-//    string name1 = typeid(doc).name();
-//    string name2 = typeid(doc).name();
-
-//    doc->setAutor(autor);
-//    doc1->setAutor(autor);
-//    doc2->setAutor(autor2);
-//    doc3->setAutor(autor3);
-//    doc4->setAutor(autor3);
-//    doc5->setAutor(autor3);
-//
-//    bib->inserirAutor(autor);
-//    bib->inserirAutor(autor2);
-//    bib->inserirAutor(autor3);
-
-
-//    bib->inserirLivro(doc2);
-//    bib->inserirLivro(doc);
-//    bib->inserirLivro(doc1);
-//    bib->inserirLivro(doc3);
-//    bib->inserirLivro(doc4);
-//    bib->inserirLivro(doc5);
 
 
     Leitor* leitor = new Leitor( "Carmon Elnor","P","9/3/2019",17);
@@ -146,7 +151,10 @@ void lerFicheiro(){
         myfile.close();
         cout<<"Dados carregados com sucesso"<<endl;
     }
-    else cout << "Unable to open file";
+    else {
+        cout << "Unable to open file| Backup loaded " ;
+        loadBackup();
+    }
 }
 
 void imprimirMenu(){
